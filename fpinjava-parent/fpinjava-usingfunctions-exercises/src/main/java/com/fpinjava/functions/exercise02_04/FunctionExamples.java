3d410b4ab102ff343d983ac42cd4f87ee705bdc2
Ice94 <mhbratek@gmail.com>
2017-11-02 23:11:44 +0100

    Solution for 2_04

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
@@ -7,7 +7,7 @@ public class FunctionExamples {
   public static final Function<Integer, Integer> square = x -> x * x;
 
   public static final Function<Function<Integer, Integer>, Function<Function<Integer, Integer>,
-                                        Function<Integer, Integer>>> compose = null; // To be implemented
+                                        Function<Integer, Integer>>> compose = x -> y -> z -> x.apply(y.apply(z)); // To be implemented
 
   public static final Function<Integer, Integer> f = compose.apply(square).apply(triple);
 }
