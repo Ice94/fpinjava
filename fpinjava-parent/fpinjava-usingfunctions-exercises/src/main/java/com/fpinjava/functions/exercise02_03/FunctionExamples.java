213f0c40f53ef831c5adef3571473ffe461a4b7d
Ice94 <mhbratek@gmail.com>
2017-11-02 22:59:26 +0100

    Solution for 2_03.

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
@@ -3,9 +3,9 @@ package com.fpinjava.functions.exercise02_03;
 
 public class FunctionExamples {
 
-  public static final Function<Integer, Function<Integer, Integer>> add = null; // To be implemented
+  public static final Function<Integer, Function<Integer, Integer>> add = x -> y -> x + y;
 
-  public static final BinaryOperator add2 =  null; // To be implemented
+  public static final BinaryOperator add2 =  arg -> arg1 -> arg + arg1;
 
-  public static final BinaryOperator mult =  null; // To be implemented
+  public static final BinaryOperator mult =  arg -> arg1 -> arg * arg1;
 }
